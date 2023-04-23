d78155c5f69fa7e528d9a8fd4b8b6df47fde7ba3
orcungurer <orcungurer@hotmail.com>
2023-04-23 19:18:01 +0300

    detail page renders component with hard-coded data

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "@/components/meetups/MeetupDetail";
+
+const MeetupDetailsPage = () => {
+  return (
+    <MeetupDetail
+      image="https://cdn1.matadornetwork.com/blogs/1/2023/02/Big-Ben-clock-social.jpg"
+      title="First Meetup"
+      address="Westminster, London, England"
+      description="This is the first one of many."
+    />
+  );
+};
+
+export default MeetupDetailsPage;
