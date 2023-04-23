b1aa64edf3e5204e7b0723a21853cec7f28d4d02
orcungurer <orcungurer@hotmail.com>
2023-04-23 19:14:35 +0300

    updated images and css of the images.

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ const DUMMY_MEETUPS = [
     id: "m1",
     title: "First Meetup",
     image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Clock_Tower_-_Palace_of_Westminster%2C_London_-_May_2007.jpg/500px-Clock_Tower_-_Palace_of_Westminster%2C_London_-_May_2007.jpg",
+      "https://cdn1.matadornetwork.com/blogs/1/2023/02/Big-Ben-clock-social.jpg",
     address: "Westminster, London, England",
     description: "This is the first one of many.",
   },
@@ -21,7 +21,7 @@ const DUMMY_MEETUPS = [
     id: "m3",
     title: "Third Meetup",
     image:
-      "https://lh3.googleusercontent.com/p/AF1QipMZji5UXvwLd-MlZsG05g_lTgBHr-PlGw5_m8Ul=s1360-w1360-h1020",
+      "https://images.prismic.io/bounce/277a1ffb-7c0d-4f6b-ad06-51682d810705_frans-ruiter-jfPGunIH_9M-unsplash.jpg?auto=compress,format",
     address: "Museumplein 6, 1071 DJ Amsterdam, Netherlands",
     description: "This is the third one of many.",
   },
