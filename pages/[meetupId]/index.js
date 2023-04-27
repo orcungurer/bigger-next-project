cbfc5702ffd0685016fc97d1dd7857b2a8cb3ec1
orcungurer <orcungurer@hotmail.com>
2023-04-27 17:59:17 +0300

    added getStaticPaths & fallback for dynamic page.

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -12,3 +12,45 @@ const MeetupDetailsPage = () => {
 };
 
 export default MeetupDetailsPage;
+
+// 2
+export const getStaticPaths = () => {
+  return {
+    fallback: false, // 3
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+      {
+        params: {
+          meetupId: "m3",
+        },
+      },
+    ],
+  };
+};
+
+// 1
+export const getStaticProps = async (context) => {
+  const meetupId = context.params.meetupId;
+
+  return {
+    props: {
+      meetupData: {
+        id: meetupId,
+        image:
+          "https://cdn1.matadornetwork.com/blogs/1/2023/02/Big-Ben-clock-social.jpg",
+        title: "First Meetup",
+        address: "Westminster, London, England",
+        description: "This is the first one of many.",
+      },
+    },
+  };
+};
