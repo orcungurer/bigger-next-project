23783fb9b35d32f35d00a89f7905101228764281
orcungurer <orcungurer@hotmail.com>
2023-04-29 00:44:57 +0300

    installed mongodb, created connection, stored data

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,10 +1,28 @@
 // POST /api/new-meetup
+import { MongoClient } from "mongodb"; // 1
 
-function handler(req, res) {
+async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
 
-    const { title, image, address, description } = data;
+    // const { title, image, address, description } = data;
+
+    // 2
+    const client = await MongoClient.connect(
+      "mongodb+srv://orcungurer:ljsrAfeGwOTjuuBN@cluster0.4nfosmu.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+
+    const db = client.db(); // 3
+
+    const meetupsCollection = db.collection("meetups"); // 4
+
+    const result = await meetupsCollection.insertOne(data); // 5
+    
+    console.log(result);
+
+    client.close(); // 6
+
+    res.status(201).json({message: "Meetup inserted!"}); // 7
   }
 }
 
