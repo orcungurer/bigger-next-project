6ee4993b5786c8f0b18bf2b6f6b74c28a2640afa
orcungurer <orcungurer@hotmail.com>
2023-04-29 18:34:37 +0300

    getting selected meetup's details from mongodb

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,12 +1,14 @@
 import MeetupDetail from "@/components/meetups/MeetupDetail";
+import { MongoClient, ObjectId } from "mongodb"; // 4, 12
 
-const MeetupDetailsPage = () => {
+const MeetupDetailsPage = (props) => {
   return (
+    // 11
     <MeetupDetail
-      image="https://cdn1.matadornetwork.com/blogs/1/2023/02/Big-Ben-clock-social.jpg"
-      title="First Meetup"
-      address="Westminster, London, England"
-      description="This is the first one of many."
+      image={props.meetupData.image}
+      title={props.meetupData.title}
+      address={props.meetupData.address}
+      description={props.meetupData.description}
     />
   );
 };
@@ -14,26 +16,26 @@ const MeetupDetailsPage = () => {
 export default MeetupDetailsPage;
 
 // 2
-export const getStaticPaths = () => {
+export const getStaticPaths = async () => {
+  // 4
+  const client = await MongoClient.connect(
+    "mongodb+srv://orcungurer:ljsrAfeGwOTjuuBN@cluster0.4nfosmu.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+  const meetupCollection = db.collection("meetups");
+
+  const meetups = await meetupCollection.find({}, { _id: 1 }).toArray(); // 5
+
+  client.close(); // 7
+
   return {
     fallback: false, // 3
-    paths: [
-      {
-        params: {
-          meetupId: "m1",
-        },
+    paths: meetups.map((meetup) => ({
+      // 6
+      params: {
+        meetupId: meetup._id.toString(),
       },
-      {
-        params: {
-          meetupId: "m2",
-        },
-      },
-      {
-        params: {
-          meetupId: "m3",
-        },
-      },
-    ],
+    })),
   };
 };
 
@@ -41,15 +43,26 @@ export const getStaticPaths = () => {
 export const getStaticProps = async (context) => {
   const meetupId = context.params.meetupId;
 
+  // 8
+  const client = await MongoClient.connect(
+    "mongodb+srv://orcungurer:ljsrAfeGwOTjuuBN@cluster0.4nfosmu.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+  const meetupCollection = db.collection("meetups");
+
+  // 9, 12
+  const selectedMeetup = await meetupCollection.findOne({
+    _id: new ObjectId(meetupId),
+  });
+
   return {
     props: {
       meetupData: {
-        id: meetupId,
-        image:
-          "https://cdn1.matadornetwork.com/blogs/1/2023/02/Big-Ben-clock-social.jpg",
-        title: "First Meetup",
-        address: "Westminster, London, England",
-        description: "This is the first one of many.",
+        id: selectedMeetup._id.toString(),
+        title: selectedMeetup.title,
+        image: selectedMeetup.image,
+        address: selectedMeetup.address,
+        description: selectedMeetup.description,
       },
     },
   };
