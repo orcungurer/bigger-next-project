11ba191d1a4cc4d8c30517b6b4192fcc67476fa6
orcungurer <orcungurer@hotmail.com>
2023-04-29 16:30:13 +0300

    sending request to our API route that uses MongoDB

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,10 +1,25 @@
 import NewMeetupForm from "@/components/meetups/NewMeetupForm";
+import { useRouter } from "next/router";
 
 const NewMeetupPage = () => {
-  const addMeetupHandler = (enteredMeetup) => {
-    console.log(enteredMeetup);
+  const router = useRouter(); // 3
+
+  const addMeetupHandler = async (enteredMeetup) => {
+    // 1
+    const response = await fetch("/api/new-meetup", {
+      method: "POST",
+      body: JSON.stringify(enteredMeetup),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json(); // 2
+    console.log(data);
+
+    router.push("/"); // 3
   };
-  
+
   return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
 };
 
